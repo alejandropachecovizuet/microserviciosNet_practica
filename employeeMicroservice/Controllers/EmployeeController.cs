4da800b9d3f21909c9a3248cf6a52a91e21edccb
Alejandro <apacheco@interware.com.mx>
2020-06-16 14:58:41 -0500

    dockerized

---
+++ employeeMicroservice/Controllers/EmployeeController.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using employeeMicroservice.Models;
+
+namespace employeeMicroservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeContext _context;
+
+        public EmployeeController(EmployeeContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Employee
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployeeItems()
+        {
+            return await _context.EmployeeItems.ToListAsync();
+        }
+
+        // GET: api/Employee/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployee(long id)
+        {
+            var employee = await _context.EmployeeItems.FindAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return employee;
+        }
+
+        // PUT: api/Employee/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmployee(long id, Employee employee)
+        {
+            if (id != employee.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(employee).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Employee
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Employee>> PostEmployee(Employee employee)
+        {
+            _context.EmployeeItems.Add(employee);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEmployee", new { id = employee.Id }, employee);
+        }
+
+        // DELETE: api/Employee/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Employee>> DeleteEmployee(long id)
+        {
+            var employee = await _context.EmployeeItems.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            _context.EmployeeItems.Remove(employee);
+            await _context.SaveChangesAsync();
+
+            return employee;
+        }
+
+        private bool EmployeeExists(long id)
+        {
+            return _context.EmployeeItems.Any(e => e.Id == id);
+        }
+    }
+}
